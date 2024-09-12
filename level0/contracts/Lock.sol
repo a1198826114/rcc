8e843f68532befff3e49ec48de3f697a87e8362e
张彧豪 <1198826114@qq.com>
2024-09-12 18:51:06 +0800

    solidity基础练习

--- level0/contracts/Lock.sol
+++ level0/contracts/Lock.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.24; //声明版本
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
 contract Lock {
+    bytes1 public a=0xb5;
     uint public unlockTime;
     address payable public owner;
 
@@ -22,8 +23,9 @@ contract Lock {
 
     function withdraw() public {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
+        //  a = 0xb5;
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp,uint8(a));
+    
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
 
